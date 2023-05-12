326be87d20e3676920a478be70fea62cff1c8a77
Nathan <sabinnat001@gmail.com>
2023-05-11 21:29:44 -0700

    added ds store to git ignore, added inquirer package

--- Unsolved/index.js
+++ Unsolved/index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquierer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
