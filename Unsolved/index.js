45b0bde7af9c84cd0114ce05404bb28a26ae487f
Nathan <sabinnat001@gmail.com>
2023-05-11 21:23:16 -0700

    added questions, started README prompt, added init, added file creation function

--- Unsolved/index.js
+++ Unsolved/index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
+const inquierer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Created an array of questions for user input
+// TODO Go back and adjust types as needed
+const questions = [
+    {
+        type: "input",
+        name: "location",
+        message: "File path"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "title"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "description"
+    },
+    {
+        type: "input",
+        name: "tableOfContents",
+        message: "table of contents"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "installation"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "usage"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "license"
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "contribution"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "test"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "questions"
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Created a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.log(err) : console.log("success")
+    })
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Created a function to initialize app
+function init() {
+    inquierer.prompt(questions).then(function(answers){
+        const markdownText = generateMarkdown(answers)
+        writeToFile(answers.location, answers)
+    })
+}
 
 // Function call to initialize app
 init();
