4a4c92098ecfeda4bf64499a7a3d32e2a4a239e0
Nathan <sabinnat001@gmail.com>
2023-05-13 14:58:54 -0700

    finished badges, more bug fixes

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# README.md-generator
+# README.md generator 
+  
+  ## Description
+
+  This project will create a "README.md" file off of user input. This project was created with JS, Node, and the inquirer package. As a developer I was able to get a better understanding of how packages work and how to create files. 
+
+  This is not a licensed project, I included that portion of the "README.md" to show it worked.
+
+  ## Table of Contents 
+
+  [Description](#description)
+
+  [Installation](#installation)
+
+  [Usage](#usage)
+
+  [Credits](#credits)
+
+  [License](#license)
+
+  [Test](#test)
+
+  [Questions](#questions)
+
+  ## Installation
+  
+  Go to this link https://github.com/nathansabin/README.md-generator and clone the repo to a folder. move over to the file and run "node.js".
+
+  ## Usage
+
+  Fill out the prompt making sure if you used a license to only use MIT, Apache, or GNU. The only other thing is to move the README.md file to the desired location.
+  
+  ## Credits
+  
+  I set up the license functions, the template, the package,  the questions, the file creation and finished the init function.
+  
+  ## License
+ 
+  ![Badge](https://img.shields.io/badge/license-MIT-blue)
+
+  https://opensource.org/license/mit/
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+  ## Test
+  
+  I made sure all badges worked as well as not entering a license. All data is entered correctly and the read me is created as well.
+
+  ## Questions
+
+  nathansabin
+
+  https://github.com/nathansabin
+
+  sabinnat001@gmail.com
+
+  
+
--- Unsolved/utils/generateMarkdown.js
+++ Unsolved/utils/generateMarkdown.js
@@ -1,12 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license == "mit"){
-    return "![MIT](https://img.shields.io/github/languages/top/lernantino/" + license + ")"
-  } else if (license == "apache") {
-    return "![apache 2.0](https://img.shields.io/github/languages/top/lernantino/" + license + ")"
+  if (license == "MIT"){
+    return "![Badge](https://img.shields.io/badge/license-MIT-blue)"
+  } else if (license == "APACHE") {
+    return "![Badge](https://img.shields.io/badge/license-Apache%202.0-brightgreen)"
   } else if (license == "gnu") {
-    return "![GNU 3.0](https://img.shields.io/github/languages/top/lernantino/" + license + ")"
+    return "![Badge](https://img.shields.io/badge/license-gnu%203.0-red)"
   } else {
     return ""
   }
@@ -15,11 +15,11 @@ function renderLicenseBadge(license) {
 // returns the license link
 // If there is no license, returns an empty string
 function renderLicenseLink(license) {
-  if (license == "mit"){
+  if (license == "MIT"){
     return "https://opensource.org/license/mit/"
-  } else if (license == "apache") {
+  } else if (license == "APACHE") {
     return "https://www.apache.org/licenses/LICENSE-2.0"
-  } else if (license == "gnu") {
+  } else if (license == "GNU") {
     return "https://www.gnu.org/licenses/gpl-3.0.en.html"
   } else {
     return ""
@@ -29,11 +29,11 @@ function renderLicenseLink(license) {
 // returns license section of README
 // If there is no license, returns an empty string
 function renderLicenseSection(license) {
-  if (license == "mit"){
+  if (license == "MIT"){
     return "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: \nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
-  } else if (license == "apache") {
+  } else if (license == "APACHE") {
     return "Licensed under the Apache License, Version 2.0 (the \"License\");\nyou may not use this file except in compliance with the License. \nYou may obtain a copy of the License at \n http://www.apache.org/licenses/LICENSE-2.0 \nUnless required by applicable law or agreed to in writing, software \ndistributed under the License is distributed on an \"AS IS\" BASIS, \nWITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. \nSee the License for the specific language governing permissions and \nlimitations under the License."
-  } else if (license == "gnu") {
+  } else if (license == "GNU") {
     return " This program is free software: you can redistribute it and/or modify \nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\nYou should have received a copy of the GNU General Public License\nalong with this program.  If not, see <https://www.gnu.org/licenses/>."
   } else {
     return ""
@@ -77,12 +77,12 @@ function generateMarkdown(data) {
   ${data.contribution}
   
   ## License
-  
-  ${renderLicenseBadge(data.license)}
+ 
+  ${renderLicenseBadge(data.license.toUpperCase())}
 
-  ${renderLicenseLink(data.license)}
+  ${renderLicenseLink(data.license.toUpperCase())}
 
-  ${renderLicenseSection(data.license)}
+  ${renderLicenseSection(data.license.toUpperCase())}
  
   ## Test
   
